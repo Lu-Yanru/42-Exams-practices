43febe1e2aed0e663898c064e464d9e9fcdcb93d
LU Yanru <39545418+Lu-Yanru@users.noreply.github.com>
2026-01-26 21:32:36 +0000

    epur_str

---
+++ Rank2/lvl3/epur_str.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+int ft_isspace(char c)
+{
+    if (c == ' ' || c == '\f' || c == '\n'
+        || c == '\r' ||  c == '\t' || c == '\v')
+        return (1);
+    else
+        return (0);
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+
+    if (argc != 2)
+    {
+        write(1, "\n", 1);
+        return (0);
+    }
+    i = 0;
+    while (argv[1][i])
+    {
+        if (ft_isspace(argv[1][i]) == 0)
+            write(1, &argv[1][i], 1);
+        else if (ft_isspace(argv[1][i]) != 0 && ft_isspace(argv[1][i - 1]) == 0
+            && i > 0 && argv[1][i + 1] != '\0')
+            write(1, " ", 1);
+        i++;
+    }
+    write(1, "\n", 1);
+}
